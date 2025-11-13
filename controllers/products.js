7b7c7474c5375fb53d57e7d8c93197ef9f7b0a28
RicardoFernandes99 <filipericardo7@hotmail.com>
2025-11-13 17:51:59 +0000

    GetProductsByID

--- controllers/products.js
+++ controllers/products.js
@@ -6,9 +6,15 @@ const router = express.Router();
 
 async function GetProducts(req, res) {
 	var data = await productService.GetProducts()
-	res.send(filterData(data.data, ["id", "prices", "compound.stock.stock", "stores.store_id"]));
+	res.send(filterData(data.data, ["id", "prices", "reference","compound.stock.stock", "description", "images.xs"]));
 }
 
+async function GetProductById(req, res) {
+	var id = req.params.id;
+	var data = await productService.GetProductById(id)
+	res.send(filterData(data, ["id", "prices", "reference","compound.stock.stock", "description", "images.xs"]));
+}
 router.get('/', GetProducts);
+router.get('/:id', GetProductById);
 
 export default router;
--- services/products.js
+++ services/products.js
@@ -3,3 +3,7 @@ import * as httpHelper from './http.js';
 export async function GetProducts() {
 	return await httpHelper.sendReq('/products', httpHelper.httpMethod.Get);
 }
+
+export async function GetProductById(id) {
+	return await httpHelper.sendReq(`/products/${id}`, httpHelper.httpMethod.Get);
+}
